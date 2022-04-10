f0cd387265c91c91ea1352e651395c36a93aeca5
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-04-09 22:43:30 -0300

    teste de condicionais e eventos

--- conversion-metrics/conversion.py
+++ conversion-metrics/conversion.py
@@ -1,10 +1,30 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Conversion Metrics'), sg.Spin(['item1', 'item2', 'item3'])],
-    [sg.Button('Convert')],
+    [sg.Text('Conversion Metrics', \
+    enable_events = True, \
+    key = '-TEXT-'), \
+    sg.Spin(['item1', 'item2', 'item3'])],
+    [sg.Button('Convert', key='-BUTTON1-')],
     [sg.Input()],
-    [sg.Text('Frutas'), sg.Button('Escolher Fruta')],
+    [sg.Text('Frutas'), sg.Button('Escolher Fruta', key='-BUTTON2-')],
 ]
 
-sg.Window('Converter', layout).read()
+window = sg.Window('Converter', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == '-BUTTON1-':
+        print('Convertendo...')
+    
+    if event == '-BUTTON2-':
+        print('Revertando...')
+
+    if event == '-TEXT-':
+        print('TEXTO...')
+
+window.close()
