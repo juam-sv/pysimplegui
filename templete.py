e43d012d12e1e265856c50a68f90f58600d8b6cc
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-05-04 22:51:34 -0300

    templete for news projects

---
+++ templete.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as sg
+
+layout = [
+]
+
+window = sg.Window('Templete', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
