c19a83a46cdbd0c7afbeaf694091a17e47b631c0
Juam Sousa <20755569+juam-sv@users.noreply.github.com>
2022-05-04 23:06:03 -0300

    update semi-final on notepad app

---
+++ 03-text-editor/main.py
@@ -0,0 +1,59 @@
+import PySimpleGUI as sg
+from pathlib import Path
+
+emotions=[
+    'happy', [':)','XD',':D','<3'],
+    'sad',[':(','T_T' ],
+    'other',[':3']
+]
+
+emotions_events = emotions[1] + emotions[3] + emotions[5]
+
+menu_layout = [
+    ['File', ['Open', 'Save', '----', 'Exit']],
+    ['Tools', ['Word Count', 'Help']],
+    ['Add', emotions]
+]
+
+sg.theme('DarkPurple')
+layout = [
+    [sg.Menu(menu_layout)],
+    [sg.Text('Not Save', key='-DOC_NAME-')],
+    [sg.Multiline(no_scrollbar=True, size = (100,20), key='-TEXTBOOK-')],
+]
+
+window = sg.Window('Text Editor', layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+    
+    if event == 'Word Count':
+        sg.popup('Word Count:', len(values['-TEXTBOOK-'].split()))
+
+    if event in emotions_events:
+        all_text = values['-TEXTBOOK-'] + ' ' + event
+        window['-TEXTBOOK-'].update(all_text)
+
+
+    if event == 'Open':
+        file_path = sg.popup_get_file('Open File', no_window=True)
+        if file_path is not None:
+            file = Path(file_path)
+            window['-TEXTBOOK-'].update(file.read_text())
+            window['-DOC_NAME-'].update(file.name)
+
+    #save and open events
+    if event == 'Save':
+        file_path = sg.popup_get_file('Save File', no_window= True, save_as=True) + '.txt'
+        file = Path(file_path)
+        file.write_text(values['-TEXTBOOK-'])
+        window['-DOC_NAME-'].update(file.name)
+
+
+
+
+
+window.close()
